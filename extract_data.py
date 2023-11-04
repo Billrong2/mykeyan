bf1deb43f5de476f14d4a59913a455dbf8fb1260
Billrong2 <52747290+Billrong2@users.noreply.github.com>
2023-11-04 02:49:00 +0000

    3000 each read for crawling

--- extract_data.py
+++ extract_data.py
@@ -1,7 +1,7 @@
 import json
 import numpy as np
-file_directory = '/workspaces/mykeyan/new_paper_result/V'
-output_directory = "/workspaces/mykeyan/parsing_target/"
+file_directory = 'new_paper_result/V'
+output_directory = "new_pattern/"
 file = []
 name = []
 data_att1 = []
@@ -11,22 +11,43 @@ dist_unknown = []
 for i in range(177):
     file.append(file_directory + str(i+1)+'.json')
     name.append("V" + str(i+1))
+out_path = output_directory + "result"+ ".json"
 out_temp = set()
 out_data = []
-line_traversaled = 0
-for file_num in range(len(file)):
-    if file_num == 139: # skip V140.json, file not found
-        file_num+=1
-    elif file_num == 169:# skip V170.json, file not found
-        file_num+=1
-    f = open(file[file_num])
-    data_temp = []
-    data_temp = json.load(f)
-    #print("file # =" + str(b)+ "\n")
-    for i in data_temp[name[file_num]]:
-        out_data.append(i)
-        line_traversaled += 1
-print(line_traversaled, len(out_data))
+#out_path = 
+#print(file[1])
+b = 0
+a = 0
+with open(out_path, 'w') as outfile:
+    for file_num in range(len(file)):
+        if file_num == 139: # skip V140.json, file not found
+            file_num+=1
+        elif file_num == 169:# skip V170.json, file not found
+            file_num+=1
+        f = open(file[file_num])
+        data_temp = []
+        data_temp= json.load(f)
+
+        b+=1
+        #print("file # =" + str(b)+ "\n")
+        for i in data_temp[name[file_num]]:
+            a+=1
+            # print("line_num:" +str(a) + "\n")
+            if("UNKNOWN" not in i['Pattern']):
+                if(i['BugDetectionTag'] in out_temp):
+                    dist_single_known.append(i)
+                elif(', ' in i['BugDetectionTag']):
+                    #dist_multi_known.append[i['BugDetectionTag']]
+                    dist_multi_known.append(i)
+                elif('[]' in i['BugDetectionTag'] ):
+                    pass
+                else:
+                    out_temp.add(i['BugDetectionTag'])
+            else:
+                dist_unknown.append(i)
+print("file # =" + str(b))
+print("Traversal lines =", a)
+
 file_written = 0
 x = len(out_data) % 4500
 x+=1
@@ -54,17 +75,5 @@ while count < x:
             outfile.write(json.dumps(i) + ",\n")
 
 
-# for i in range(count):
-#     out_path = output_directory + str(i)+ ".json"
-#     with open(out_path, 'w') as outfile:
-#         temp = 0
-#         for i in out_data[count]:
-#             temp+=1
-#             file_written +=1
-#             if temp ==1:
-#                 outfile.write(("{\""+ "Single" + "\":[" + json.dumps(i) + ",\n" ))
-#             elif temp ==len(out_data[count]):
-#                 outfile.write(json.dumps(i) + "]}"+ "\n")
-#             else:
-#                 outfile.write(json.dumps(i) + ",\n")
+
 print(file_written)
