bd2b61e647e3569efb3ba39d12cb097d710daf0c
Billrong2 <52747290+Billrong2@users.noreply.github.com>
2023-11-07 05:31:54 +0000

    new download

--- downloader.py
+++ downloader.py
@@ -8,18 +8,13 @@ file = '/workspaces/mykeyan/new_pattern/UNKNOWN to Try/UNKNOWN to Try 2.json'
 output_directory = "/workspaces/mykeyan/downloaded_result/UNKNOWN to Try 2.json"
 f = open(file)
 data = json.load(f)['UNKNOWN to Try']
-
-travseled_file = set()
-
 for i in range(len(data)):
     current_url = data[i]['Url']+"/commits/" + data[i]['Fixed commit']
-    my_command = "curl --request GET \--url \"" + current_url + "\" \\--header \"Authorization: Bearer TOKEN\" \\--header \"X-GitHub-Api-Version: 2022-11-28\" "
+    my_command = "curl --request GET \--url \"" + current_url + "\" \\--header \"Authorization: Bearer Token\" \\--header \"X-GitHub-Api-Version: 2022-11-28\" "
     x = data[i]["FileName"].split('.')
     f_temp = x[-2]+'.'+x[-1]
     response = subprocess.check_output(my_command, shell=True, text=True)
     commit_data = json.loads(response)
-    
-    # print('RUNNING', a)
     if "files" in commit_data:
         for j in range(len(commit_data["files"])):
             if f_temp in commit_data["files"][j]["filename"]:
@@ -31,16 +26,15 @@ for i in range(len(data)):
             else:
                 pass
         response = requests.get(raw_url)
-        print(response.content)
+        response_data = response.text
         if response.status_code == 200:
-            data[i]["Complete After Code"] = str(response.content)
+            data[i]["Complete After Code"] = response_data
         else:
             data[i]["Complete After Code"] = "Missing Raw Link for After Code"
     else:
         pass
     print(str(i) +"traversal complete \n")
-
-
+    time.sleep(0.5)
 temp = 0
 a = 0
 with open(output_directory, 'w') as outfile:
